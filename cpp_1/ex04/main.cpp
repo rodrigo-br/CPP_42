b67e6566362827a4583d49dfbe1a6f3ac768fe59
rodrigo-br <rodrigoab123@gmail.com>
2022-11-12 21:20:42 -0300

    cpp_1 ex04 done

---
+++ cpp_1/ex04/main.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+	if (argc != 4) {
+		std::cerr << "wrong args" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	std::string		file = argv[1];
+	std::ifstream	ifs(file, std::ifstream::in);
+	if (ifs.fail()) {
+		std::cerr << "wrong file" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	std::ofstream	ofs(file + ".replace", std::ofstream::out | std::ofstream::app);
+	std::string		s1 = argv[2];
+	std::string		s2 = argv[3];
+	std::string		line;
+	size_t			pos;
+	while (!ifs.eof())
+	{
+		std::getline(ifs, line);
+		pos = line.find(s1, 0);
+		if (pos < line.length())
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+		}
+		ofs << line << std::endl;
+	}
+	return (EXIT_SUCCESS);
+}
