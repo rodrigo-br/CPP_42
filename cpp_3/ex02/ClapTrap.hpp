4248028623a944bca1ab7051e6476afa49ab323e
rodrigo-br <rodrigoab123@gmail.com>
2022-11-17 21:46:30 -0300

    cpp_3 ex02 done

---
+++ cpp_3/ex02/ClapTrap.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+	private:
+		static std::string const	_type;
+		
+	protected:
+		std::string	const	_name;
+		int					_hitPoints;
+		int					_energyPoints;
+		int					_attackDmg;
+
+	public:
+		ClapTrap();
+		ClapTrap(std::string const name);
+		ClapTrap(ClapTrap const &src);
+		~ClapTrap();
+
+		ClapTrap &operator=(ClapTrap const &rhs);
+
+		void 		attack(const std::string& target);
+		void 		takeDamage(unsigned int amount);
+		void 		beRepaired(unsigned int amount);
+		std::string getName() const;
+		int			getHP() const;
+		int			getEP() const;
+		int			getDMG() const;
+		void		setHP(unsigned int value);
+		void		setEP(unsigned int value);
+		void		setDMG(unsigned int value);
+};
+
+std::ostream	&operator<<(std::ostream &o, ClapTrap const &value);
+
+#endif
