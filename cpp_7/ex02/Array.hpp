668e0aebf409795495ec63f2cf6c1306dbe5a1ab
rodrigo-br <rodrigoab123@gmail.com>
2022-11-26 23:01:39 -0300

    cpp 7 not started yet

---
+++ cpp_7/ex02/Array.hpp
@@ -0,0 +1,24 @@
+#ifndef ARRAY_H
+# define ARRAY_H
+
+#include <iostream>
+
+template<typename T>class Array
+{
+	private:
+		
+
+	public:
+
+		Array();
+		~Array();
+
+};
+
+Array::Array() {
+}
+
+Array::~Array() {
+}
+
+#endif
---
+++ cpp_7/ex02/main.cpp
@@ -0,0 +1,25 @@
+#include "iter.hpp"
+
+void print_line() {
+	std::cout << std::string(42, '-') << std::endl;
+}
+
+void t(std::string msg) {
+	std::cout << "\t\t" << msg << std::endl;
+}
+
+int main(void)
+{
+	{
+		
+	}
+
+	{
+		
+	}
+
+	{
+		
+	}
+	
+}
