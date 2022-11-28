a53bce7c467681384113a34011962a8c627af585
rodrigo-br <rodrigoab123@gmail.com>
2022-11-28 14:03:20 -0300

    só quero ir pra casa

--- cpp_7/ex02/Array.hpp
+++ cpp_7/ex02/Array.hpp
@@ -6,19 +6,50 @@
 template<typename T>class Array
 {
 	private:
-		
+		T		*_array;
+		size_t	_size;
 
 	public:
 
-		Array();
-		~Array();
+		Array<T>() : _array(NULL), _size(0) {};
+
+		Array<T>(unsigned int n) : _size(n) {
+			_array = new T[n];
+		};
+
+		Array<T>(Array<T> const &src) {
+			*this = src;
+		};
+
+		~Array<T>() {
+			if (_array)
+				delete[] _array;
+		};
+
+		T	&operator[](unsigned int i) {
+			if (i >= 0 && i < _size)
+				return _array[i];
+			throw WrongIndexException();
+		};
+
+		Array<T> &operator=(Array<T> const &src) {
+			_size = src.size();
+			_array = new T[_size];
+			for (unsigned int i=0;i<_size;i++) {
+				_array[i] = src._array[i];
+			}
+			return *this;
+		};
+
+		size_t		size() const { return _size; };
+
+		class WrongIndexException : public std::exception {
+			public :
+				virtual const char *what() const throw() {
+					return "Ur doing it wrong\n";
+				};
+		};
 
 };
-
-Array::Array() {
-}
-
-Array::~Array() {
-}
 
 #endif
--- cpp_7/ex02/main.cpp
+++ cpp_7/ex02/main.cpp
@@ -1,25 +1,59 @@
-#include "iter.hpp"
+#include "Array.hpp"
 
 void print_line() {
 	std::cout << std::string(42, '-') << std::endl;
 }
 
 void t(std::string msg) {
-	std::cout << "\t\t" << msg << std::endl;
+	std::cout << "\t" << msg << std::endl;
+}
+
+template<typename T>
+void	print_array(Array<T> a) {
+	print_line();
+	for (unsigned int i=0;i<a.size();i++) {
+		std::cout << a[i] << std::endl;
+	}
 }
 
 int main(void)
 {
-	{
-		
+	Array<unsigned int> a(5);
+	for (int i=0;i<5;i++) {
+		a[i] = i;
 	}
+	print_line();t("reassign array values");print_array(a);
 
-	{
-		
-	}
+	Array<unsigned int> copy(a);
+	print_line();t("testing copy constructor");print_array(copy);
 
-	{
-		
+	Array<unsigned int> assign = Array<unsigned int>(a);
+	print_line();t("testing assign OP");print_array(assign);
+
+	a[0] = 11;
+	copy[0] = 22;
+	assign[0] = 33;
+	print_line();t("testing independency");
+	print_array(a);print_array(copy);print_array(assign);
+
+	Array<std::string> str(5);
+	str[0] = "A";
+	str[1] = "Emily";
+	str[2] = "é";
+	str[3] = "uma";
+	str[4] = "cachorra";
+	print_line();t("Another type of Array");print_array(str);
+
+	print_line();t("Testing invalid indexes");print_line();
+	try {
+		std::cout << str[5] << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what();
 	}
-	
+	print_line();
+	try {
+		std::cout << str[-1] << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what();
+	}
 }
