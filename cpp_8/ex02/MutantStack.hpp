361e6c5d98398179a6043fcaf831cfad6f255af2
rodrigo-br <rodrigoab123@gmail.com>
2022-11-29 12:47:20 -0300

    cpp8 ex02 done?

--- cpp_8/ex02/MutantStack.hpp
+++ cpp_8/ex02/MutantStack.hpp
@@ -17,6 +17,13 @@ class MutantStack : public std::stack<T, Container>
 		MutantStack() {};
 		~MutantStack() {};
 
+		typedef typename Container::iterator iterator;
+		typedef typename Container::reverse_iterator reverse_iterator;
+		iterator begin() { return this->c.begin(); }
+		iterator end() { return this->c.end(); }
+		reverse_iterator rbegin() { return this->c.rbegin(); }
+		reverse_iterator rend() { return this->c.rend(); }
+
 };
 
 #endif
--- cpp_8/ex02/main.cpp
+++ cpp_8/ex02/main.cpp
@@ -1,9 +1,57 @@
 #include "MutantStack.hpp"
+#include <list>
+
+void print_line() {
+	std::cout << std::string(42, '-') << std::endl;
+}
 
 int main (void)
 {
-	MutantStack<int> mstack;
-
-	mstack.push(5);
-	std::cout << mstack.top() << std::endl;
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	print_line();
+	{
+		std::list<int> lst;
+		lst.push_back(5);
+		lst.push_back(17);
+		std::cout << lst.back() << std::endl;
+		lst.pop_back();
+		std::cout << lst.size() << std::endl;
+		lst.push_back(3);
+		lst.push_back(5);
+		lst.push_back(737);
+		//[...]
+		lst.push_back(0);
+		std::list<int>::iterator it = lst.begin();
+		std::list<int>::iterator ite = lst.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::list<int> s(lst);
+	}
 }
